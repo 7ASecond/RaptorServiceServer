45daa86e295347bf1b91643e360b81cee40d1720
7ASecond-Net <admin@7ASecond.net>
2015-07-01 00:35:44 +0100

    2

--- RaptorServiceServer/Program.cs
+++ RaptorServiceServer/Program.cs
@@ -16,7 +16,7 @@ namespace RaptorServiceServer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
