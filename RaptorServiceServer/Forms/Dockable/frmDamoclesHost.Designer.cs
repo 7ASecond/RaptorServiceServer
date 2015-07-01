57673acb89bdc0938c26a5f93078f7e33f681637
7ASecond <admin@7ASecond.net>
2015-07-01 22:28:33 +0100

    MEnu and Toolbar designed

--- RaptorServiceServer/Forms/Dockable/frmDamoclesHost.Designer.cs
+++ RaptorServiceServer/Forms/Dockable/frmDamoclesHost.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 123);
+            this.ClientSize = new System.Drawing.Size(441, 234);
             this.Name = "frmDamoclesHost";
             this.Text = "frmDamoclesHost";
             this.ResumeLayout(false);
